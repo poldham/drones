95670043720db491f36342f0e85fe75ad9dabe22
Paul Oldham <poldham@mac.com>
2018-08-15 11:17:53 +0100

    delete data raw and ignore from repo

--- ignore/related_applicationscode.R
+++
@@ -1,4 +0,0 @@
-numbers %>%
-  separate_rows(related_application_numbers, sep = ";") %>%
-  mutate(related_application_numbers = str_replace_all(related_application_numbers, ",", " ")) %>%
-  select(related_application_numbers) %>% View()
